60c8755ffd6b97454c1effc6056925f5e0f170ff
lenarsharipov <lenarsharipov@gmail.com>
2023-02-07 17:02:09 +0300

    3. Денежные переводы AccountStorage [#1104]

---
+++ src/main/java/ru/job4j/cache/Account.java
@@ -0,0 +1,7 @@
+package ru.job4j.cache;
+
+public record Account(int id, int amount) {
+    public static Account of(int id, int amount) {
+        return new Account(id, amount);
+    }
+}
---
+++ src/main/java/ru/job4j/cache/AccountStorage.java
@@ -0,0 +1,61 @@
+package ru.job4j.cache;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+public final class AccountStorage {
+    private final HashMap<Integer, Account> accounts = new HashMap<>();
+
+    public boolean add(Account account) {
+        synchronized (accounts) {
+            var rsl = false;
+            if (accounts.get(account.id()) == null) {
+                var tempAccount = Account.of(account.id(), account.amount());
+                accounts.put(account.id(), tempAccount);
+                rsl = true;
+            }
+            return rsl;
+        }
+    }
+
+    public boolean update(Account account) {
+        synchronized (accounts) {
+            var rsl = false;
+            if (accounts.get(account.id()) != null) {
+                accounts.put(account.id(), Account.of(account.id(), account.amount()));
+                rsl = true;
+            }
+            return rsl;
+        }
+    }
+
+    public boolean delete(int id) {
+        synchronized (accounts) {
+            return accounts.remove(id, accounts.get(id));
+        }
+    }
+
+    public Optional<Account> getById(int id) {
+        synchronized (accounts) {
+            var rsl = accounts.get(id);
+            if (rsl != null) {
+                rsl = Account.of(id, rsl.amount());
+            }
+            return Optional.ofNullable(rsl);
+        }
+    }
+
+    public boolean transfer(int fromId, int toId, int amount) {
+        synchronized (accounts) {
+            var rsl = false;
+            if (accounts.get(fromId) != null
+                    && accounts.get(toId) != null
+                    && accounts.get(fromId).amount() >= amount) {
+                accounts.put(toId, Account.of(toId, accounts.get(toId).amount() + amount));
+                accounts.put(fromId, Account.of(fromId, accounts.get(fromId).amount() - amount));
+                rsl = true;
+            }
+            return rsl;
+        }
+    }
+}
