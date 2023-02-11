579b3b343790b7fdfe78c75e2202dc240b991afd
lenarsharipov <lenarsharipov@gmail.com>
2023-02-11 13:27:31 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,45 @@
+package ru.job4j.buffer;
+
+import ru.job4j.SimpleBlockingQueue;
+
+/**
+ * Класс ParallelSearch - утилита по поиску текста в файловой системе.
+ * Утилита предназначена для работы в многопоточной среде.
+ * Одна нить ищет файлы с подходящим именем.
+ * Вторая нить берет эти файлы и читает их.
+ *
+ * @author Lenar Sharipov
+ * @version 1.0
+ */
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        var queue = new SimpleBlockingQueue<Integer>(10);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+
+        new Thread(
+                () -> {
+                    for (var index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        ).start();
+    }
+}
