eaf79b3f28844e972340d564c73e1b7672c6120e
lenarsharipov <lenarsharipov@gmail.com>
2023-02-12 12:39:07 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -2,6 +2,10 @@ package ru.job4j;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.*;
 
 class SimpleBlockingQueueTest {
@@ -99,4 +103,76 @@ class SimpleBlockingQueueTest {
         assertThat(queue.poll()).isEqualTo(5);
     }
 
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        Thread producer = new Thread(
+                () -> IntStream.range(0, 5).forEach(
+                        el -> {
+                            try {
+                                queue.offer(el);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                )
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).isEqualTo(Arrays.asList(0, 1, 2, 3, 4));
+    }
+
+    @Test
+    public void whenFetchEvenThenGetIt() throws InterruptedException {
+        var queue = new SimpleBlockingQueue<>(10);
+        var buffer = new CopyOnWriteArrayList<>();
+        var producer = new Thread(
+                () -> IntStream.range(0, 10).forEach(
+                        el -> {
+                            try {
+                                queue.offer(el);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                )
+        );
+        producer.start();
+        var consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                            queue.poll();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).isEqualTo(Arrays.asList(0, 2, 4, 6, 8));
+    }
 }
