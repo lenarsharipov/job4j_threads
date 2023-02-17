cd1aee085f5fcdf83a5a2568354be05753d6df7f
lenarsharipov <lenarsharipov@gmail.com>
2023-02-17 14:01:02 +0300

    2. ExecutorService рассылка почты. [#63097 #392573]

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -40,6 +40,13 @@ public class EmailNotification {
      */
     public void close() {
         pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) {
